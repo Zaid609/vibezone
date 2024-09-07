c3beadbd1e864d8e620a9c7a23c7076d16901412
Kojo83 <freshcollardon@gmail.com>
2024-09-07 10:33:02 +0000

    Fix typo: strickQuery to strictQuery in MongoDB connection

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,45 +1,43 @@
 import { Webhook } from 'svix';
 import { headers } from 'next/headers';
-import { WebhookEvent } from '@clerk/nextjs/server';
-import { createOrUpdateUser, deleteUser } from '@lib/actions/user';  // Ensure deleteUser is imported
+import { createOrUpdateUser, deleteUser } from '@lib/actions/user';
 
 export async function POST(req) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local');
   }
 
-  // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get('svix-id');
   const svix_timestamp = headerPayload.get('svix-timestamp');
   const svix_signature = headerPayload.get('svix-signature');
 
-  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occurred -- no svix headers', {
       status: 400,
     });
   }
 
-  // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
-
   let evt;
 
-  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       'svix-id': svix_id,
       'svix-timestamp': svix_timestamp,
       'svix-signature': svix_signature,
     });
+
+    // Add console logs here to debug
+    console.log("Webhook received:", evt); // Log the full webhook event
+    const { id, first_name, last_name, image_url, email_addresses, username } = evt?.data;
+    console.log("User Data:", { id, first_name, last_name, email_addresses, username }); // Log the user data
+
   } catch (err) {
     console.error('Error verifying webhook:', err);
     return new Response('Error occurred', {
@@ -47,7 +45,6 @@ export async function POST(req) {
     });
   }
 
-  // Handle the event
   const eventType = evt?.type;
 
   if (eventType === 'user.created' || eventType === 'user.updated') {
@@ -67,7 +64,6 @@ export async function POST(req) {
     }
   }
 
-  // Handle the user.deleted event
   if (eventType === 'user.deleted') {
     const { id } = evt?.data;
 
@@ -85,7 +81,6 @@ export async function POST(req) {
     }
   }
 
-  // Handle other event types or unknown events
   return new Response('Event not handled', {
     status: 200,
   });
